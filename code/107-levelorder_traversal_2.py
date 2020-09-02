5ce6d52a5aba953a7fa41f3e901ac6537f622403
gjt9274 <gjt9274@gmail.com>
2020-09-02 09:03:48 +0800

    easy

--- code/107-levelorder_traversal_2.py
+++ code/107-levelorder_traversal_2.py
@@ -6,22 +6,23 @@
 #         self.right = None
 
 class Solution:
-    def levelOrder(self, root: TreeNode) -> List[List[int]]:
+    def levelOrderBottom(self, root: TreeNode) -> List[List[int]]:
         if not root:
             return []
-
-        res = []
         queue = []
+        res = []
         queue.append(root)
         while queue:
+            n = len(queue)
             tmp = []
-            n = len(queue) # 需要记录当前层的结点个数，因为无法区分哪些节点属于哪一层
             for i in range(n):
                 node = queue.pop(0)
                 tmp.append(node.val)
                 if node.left:
-                    queue.append(node.left)
+                  queue.append(node.left)
                 if node.right:
-                    queue.append(node.right)
+                  queue.append(node.right)
+
             res.append(tmp)
-        return res
+
+        return res[::-1]
