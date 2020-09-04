d72ce96c6a904f28a5dc354b5e65d7ada874e584
gjt9274 <gjt9274@gmail.com>
2020-09-05 00:09:28 +0800

    easy

--- code/83-remove_duplicates_from_sorted_list.py
+++ code/83-remove_duplicates_from_sorted_list.py
@@ -11,10 +11,9 @@ class Solution:
 
         cur = head
         while cur and cur.next:
-            while cur and cur.next:
-                if cur.val == cur.next.val:
-                    cur.next = cur.next.next
-                else:
-                    cur = cur.next
+            if cur.val == cur.next.val:
+                cur.next = cur.next.next
+            else:
+                cur = cur.next
 
         return head
