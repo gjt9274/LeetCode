07d4f759bd936c3a71df7f9504b4d6aefcc29c26
gjt9274 <gjt9274@gmail.com>
2020-09-14 11:23:34 +0800

    medium

---
+++ code/338-counting_bits.py
@@ -0,0 +1,29 @@
+
+# 方法1： 直接计算每个数的二进制数 1 的数目
+class Solution:
+    def countBits(self, num: int) -> List[int]:
+
+        def count(n):
+            cnt = 0
+            while n != 0:
+                n = n & (n-1)
+                cnt += 1
+            return cnt
+
+        res = [0] * (num+1)
+        for i in range(num+1):
+            res[i] = count(i)
+        return res
+
+#方法2： 动态规划
+
+
+class Solution:
+    def countBits(self, num: int) -> List[int]:
+        res = [0]*(num+1)
+        for i in range(num+1):
+            if (i & 1) == 0:
+                res[i] = res[i//2]
+            else:
+                res[i] = res[i-1] + 1
+        return res
