665a6f0aadf0309346b317aabc5d1c67c03fa752
gjt9274 <gjt9274@gmail.com>
2020-09-26 21:00:27 +0800

    medium\

---
+++ code/3-longest_substring_without_repeating_characters.py
@@ -0,0 +1,39 @@
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        if len(s) < 2:
+            return len(s)
+
+        win_dict = defaultdict(lambda: 0)
+        left, right, maxLen = 0, 0, 0
+        while left < len(s):
+            while right < len(s) and win_dict[s[right]] == 0:
+                win_dict[s[right]] = 1
+                right += 1
+            # if right == len(s):
+            #     return right - left
+            if right - left > maxLen:
+                maxLen = right - left
+            left += 1
+            right = left
+            win_dict.clear()
+
+        return maxLen
+
+
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        if len(s) < 2:
+            return len(s)
+
+        lookup = set()
+        maxLen = 0
+        right = -1
+        for left in range(len(s)):
+            if left != 0:
+                lookup.remove(s[left-1])
+            while right+1 < len(s) and s[right+1] not in lookup:
+                lookup.add(s[right+1])
+                right += 1
+            maxLen = max(maxLen, right-left + 1)
+
+        return maxLen
