dff4a3fdd5c1af650d732694eaf93ccb8201653e
gjt9274 <gjt9274@gmail.com>
2020-10-27 20:44:20 +0800

    medium

--- queue_and_stack/4-perfectSquares.java
+++ queue_and_stack/4-perfectSquares.java
@@ -1,5 +1,27 @@
 class Solution {
     public int numSquares(int n) {
+        Set<Integer> queue = new LinkedList<>();
+        HashSet<Integer> visited = new HashSet<>();
+        queue.offer(n);
+        visited.add(n);
+        int level = 0;
+
+        while(!queue.isEmpty()){
+            level++;
+            int len = queue.size();
+            for(int i = 0; i < len; i++){
+                int cur = queue.poll();
+                for(int j = 1; j*j <= cur; j++){
+                    int tmp = cur - j*j;
+                    if(tmp == 0) {return level;}
+                    if(!visited.contains(tmp)){
+                        queue.offer(tmp);
+                    }
+                    visited.add(tmp);
+                }
+            }
+        }
+        return level;
         
     }
 }
